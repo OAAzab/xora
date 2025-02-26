f2d2541ed9ef9c3abed574dc6076052bc573065b
Omar Azab <oaazab10@gmail.com>
2025-02-26 18:44:03 +0200

    xora

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
             <p className="opacity-70">
-              <a href="https:/oazab.vercel.app/" target="blank">OAzab</a></p>
+              <a href="https:/oazab.vercel.app/" target="_blank">OAzab</a></p>
           </div>
           <div className="flex items-center justify-center sm:ml-auto">
             <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
